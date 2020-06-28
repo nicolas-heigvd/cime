82093e84008d66c96027ac8065dbc56694429b93
nicolas.heigvd <nicolas.blanc1@heig-vd.ch>
2020-06-28 15:57:46 +0200

    Update geo script.

--- geotiff2obj.sh
+++ geotiff2obj.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-for f0 in ./data/PROCESSED/output_4326_*.tiff; do
+basedir="./DATA/PROCESSED/"
+for f0 in "${basedir}"ZONE${1}/output_4326_*.tiff; do
   echo "Processing ${f0}"
   f=${f0##*/}
-  tin-terrain dem2tin --input "${f0}" --output ./data/PROCESSED/OBJ/"${f%.*}".obj
+  tin-terrain dem2tin --input "${f0}" --output "${basedir}"ZONE${1}/OBJ/"${f%.*}".obj
 done
