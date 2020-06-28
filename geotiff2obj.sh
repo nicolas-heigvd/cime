9c8fe97105591f13a6bc1fd0a743455e18362bb2
nicolas.heigvd <nicolas.blanc1@heig-vd.ch>
2020-06-28 16:01:31 +0200

    Update geo script.

--- geotiff2obj.sh
+++ geotiff2obj.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-basedir="./DATA/PROCESSED/"
-for f0 in "${basedir}"ZONE${1}/output_4326_*.tiff; do
-  echo "Processing ${f0}"
-  f=${f0##*/}
-  tin-terrain dem2tin --input "${f0}" --output "${basedir}"ZONE${1}/OBJ/"${f%.*}".obj
-done
+if [ $# -eq 0 ]
+then
+    basedir="./DATA/PROCESSED/"
+    mkdir -p "${basedir}"ZONE${1}/OBJ/
+
+    for f0 in "${basedir}"ZONE${1}/output_4326_*.tiff; do
+      echo "Processing ${f0}"
+      f=${f0##*/}
+      tin-terrain dem2tin --input "${f0}" --output "${basedir}"ZONE${1}/OBJ/"${f%.*}".obj
+    done
+else
+    echo "Please, give the area number as an argument."
+fi
