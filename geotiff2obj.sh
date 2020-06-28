11e36abfe21f2ef66182c78364504eaf134479cf
nicolas.heigvd <nicolas.blanc1@heig-vd.ch>
2020-06-28 17:03:47 +0200

    Update geo script.

--- geotiff2obj.sh
+++ geotiff2obj.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-if [ $# -eq 1 ]
+if [ $# -ne 1 ]
 then
+    echo "Please, give the area number as an argument."
+else
     basedir="/data/DATA/PROCESSED/"
     mkdir -p "${basedir}"ZONE${1}/OBJ/
 
@@ -10,6 +12,4 @@ then
       f=${f0##*/}
       tin-terrain dem2tin --input "${f0}" --output "${basedir}"ZONE${1}/OBJ/"${f%.*}".obj
     done
-else
-    echo "Please, give the area number as an argument."
 fi
