013d9c5c155c294a0e64f3b28419b45b3c608cfa
nicolas.heigvd <nicolas.blanc1@heig-vd.ch>
2020-06-28 19:35:26 +0200

    Minor fix.

--- start_tin-terrain.sh
+++ start_tin-terrain.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 
-sudo docker-compose --remove-orphans -f docker-compose.yml down -v;
+sudo docker-compose -f docker-compose.yml down -v;
 sudo docker-compose -f docker-compose.yml up;
 
